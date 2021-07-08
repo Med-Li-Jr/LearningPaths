c891d3f9d2ec48711787f4c68ec833efc9c76f76
med-li-jr <li.med.jr@gmail.com>
2021-07-08 09:54:55 -0700

    Learning SignalR net core

---
+++ Csharp/LearningSignalR/LearningSignalR/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using LearningSignalR.Hubs;
+using LearningSignalR.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public HomeController(IHubContext<NotificationHub> hubContext, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Privacy(string user, string messageString)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", user, messageString);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
